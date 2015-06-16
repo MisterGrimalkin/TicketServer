710f223bf5eaf653b71fef012d456f4a46480114
MisterGrimalkin <loki@amarantha.net>
2015-06-16 09:19:06 +0100

    Mostly cool

--- ticketserver.sh
+++ ticketserver.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
+#-Dcom.sun.management.jmxremote \
+#-Dcom.sun.management.jmxremote.port=8855 \
+#-Dcom.sun.management.jmxremote.local.only=false \
+#-Dcom.sun.management.jmxremote.authenticate=false \
+#-Dcom.sun.management.jmxremote.ssl=false \
 java \
--Dcom.sun.management.jmxremote \
--Dcom.sun.management.jmxremote.port=8855 \
--Dcom.sun.management.jmxremote.local.only=false \
--Dcom.sun.management.jmxremote.authenticate=false \
--Dcom.sun.management.jmxremote.ssl=false \
 -Djava.library.path=../LightBoard/native/libpi4j.so \
--cp "../LightBoard/lib/*:lib/*:target/classes" \
+-cp "../lightboard/lib/*:lib/*:target/classes" \
 net.amarantha.ticketserver.Main $*
